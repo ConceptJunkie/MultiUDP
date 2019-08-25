2fa9578d1087e823d39bf45053c2c0bba1bbd7c1
Rick Gutleber <rickg@his.com>
2019-08-25 10:25:58 -0400

    forgot to add the new files

---
+++ ConcurrentQueue.h
@@ -0,0 +1,117 @@
+//******************************************************************************
+//
+//  concurrentQueue.h
+//
+//  adapted from https://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
+//
+//******************************************************************************
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+
+//******************************************************************************
+//
+//  class concurrentQueue
+//
+//******************************************************************************
+
+template< typename Data >
+class concurrentQueue {
+    private:
+        std::queue< Data > m_queue;
+        mutable std::mutex m_mutex;
+        std::condition_variable_any m_condition;
+
+    public:
+        concurrentQueue & operator =( const concurrentQueue & ) = delete;
+
+        void push( const Data & data ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+            m_queue.push( data );
+            lock.unlock( );
+
+            m_condition.notify_one( );
+        }
+
+        bool tryPop( Data & value ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+
+            if ( m_queue.empty( ) ) {
+                return false;
+            }
+
+            value = m_queue.front( );
+            m_queue.pop( );
+
+            return true;
+        }
+
+        void waitAndPop( Data & value ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+
+            while ( m_queue.empty( ) ) {
+                m_condition.wait( lock );
+            }
+
+            value = m_queue.front( );
+            m_queue.pop( );
+        }
+
+};
+
+
+//******************************************************************************
+//
+//  class concurrentPriorityQueue
+//
+//******************************************************************************
+
+template< typename Data, typename Compare >
+class concurrentPriorityQueue {
+    private:
+        std::priority_queue< Data, std::vector< Data >, Compare > m_queue;
+        mutable std::mutex m_mutex;
+        std::condition_variable_any m_condition;
+
+    public:
+        concurrentPriorityQueue & operator =( const concurrentPriorityQueue & ) = delete;
+
+        void push( const Data & data ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+            m_queue.push( data );
+            lock.unlock( );
+
+            m_condition.notify_one( );
+        }
+
+        bool tryPop( Data & value ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+
+            if ( m_queue.empty( ) ) {
+                return false;
+            }
+
+            value = m_queue.front( );
+            m_queue.pop( );
+
+            return true;
+        }
+
+        // We are waiting for a specific index to show up, because we want to
+        // output the packets in order, so we watch the index of the top item
+        // in the priority queue.
+
+        const Data & waitForIndexAndPop( int nIndex, Data & value ) {
+            std::unique_lock< std::mutex > lock( m_mutex );
+
+            while ( m_queue.empty( ) || ( m_queue.top( ).index != nIndex ) ) {
+                m_condition.wait( lock );
+            }
+
+            value = m_queue.top( );
+            m_queue.pop( );
+        }
+};
+
---
+++ PacketData.h
@@ -0,0 +1,40 @@
+//******************************************************************************
+//
+//  PacketData.h
+//
+//******************************************************************************
+
+#include <string.h>
+
+constexpr int PACKET_DATA_SIZE = 512;    // Max length of buffer
+
+
+//******************************************************************************
+//
+//  struct PacketData
+//
+//******************************************************************************
+
+struct PacketData {
+    char buffer[ PACKET_DATA_SIZE ];
+    int index;
+    int used;
+
+    PacketData( int _index = 0 ) {
+        index = _index;
+        used = 0;
+        bzero( buffer, PACKET_DATA_SIZE );
+    }
+
+    constexpr int getSize( ) const {
+        return PACKET_DATA_SIZE;
+    }
+};
+
+
+struct PacketDataIndexLessThan {
+    bool operator( ) ( const PacketData & lhs, const PacketData & rhs ) const {
+        return lhs.index < rhs.index;
+    }
+};
+
