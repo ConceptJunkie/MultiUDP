07d212f6b0a77ad6f6fc23db3529f9cd8d4cca6b
Rick Gutleber <rgutleber@idirect.net>
2019-08-29 15:22:43 -0400

    another unit test for ConcurrentQueue

--- ConcurrentQueueTest.cpp
+++ ConcurrentQueueTest.cpp
@@ -2,6 +2,8 @@
 //
 //  ConcurrentQueue unit tests
 //
+//  TODO:  use gtest...
+//
 //******************************************************************************
 
 #include <algorithm>
@@ -16,13 +18,13 @@
 
 //******************************************************************************
 //
-//  processData
+//  insertData
 //
 //******************************************************************************
 
 void insertData( int nPrefix, int nItemsPerThread, concurrentQueue< int > & testQueue ) {
 	for ( int i = 0; i < nItemsPerThread; i++ ) {
-        testQueue.push( nPrefix * nItemsPerThread + 1 );
+        testQueue.push( nPrefix * nItemsPerThread + i );
     }
 }
 
@@ -47,22 +49,89 @@ bool testInserting( int nThreads, int nItemsPerThread ) {
 
 	std::vector< int > items;
 
-	while ( true ) {
-		int nItem;
+	int nItem;
 
-		if ( !testQueue.tryPop( nItem ) ) {
-			break;
-		}
-
+	while ( testQueue.tryPop( nItem ) ) {
 		items.push_back( nItem );
 	}
 
 	std::sort( items.begin( ), items.end( ) );
 
 	if ( items.size( ) != nThreads * nItemsPerThread ) {
-		std::cout << "Found " << items.size( ) << " items, but expected " <<
+		std::cout << "test1:  Found " << items.size( ) << " items, but expected " <<
 		              nThreads * nItemsPerThread << std::endl;
 		bSuccess = false;
+	}
+
+    for ( int i = 0; i < nThreads * nItemsPerThread; i++ ) {
+		if ( items[ i ] != i ) {
+			std::cout << "test2:  Somehow, we don't have the items we inserted!" << std::endl;
+			bSuccess = false;
+			break;
+		}
+	}
+
+	return bSuccess;
+}
+
+
+//******************************************************************************
+//
+//  popData
+//
+//******************************************************************************
+
+void popData( concurrentQueue< int > & testQueue, std::vector< int > & items ) {
+	int nItem;
+
+	while ( testQueue.tryPop( nItem ) ) {
+		items.push_back( nItem );
+    }
+}
+
+
+//******************************************************************************
+//
+//  testPopping
+//
+//******************************************************************************
+
+bool testPopping( int nThreads, int nItems ) {
+	bool bSuccess = true;
+
+    concurrentQueue< int > testQueue;
+
+    for ( int i = 0; i < nItems; i++ ) {
+        testQueue.push( i );
+    }
+
+    std::vector< std::thread > threads( nThreads );
+	std::vector< std::vector< int > > itemVectors( nThreads );
+
+    for ( int i = 0; i < nThreads; i++ ) {
+        threads[ i ] = std::thread( popData, std::ref( testQueue ), std::ref( itemVectors[ i ] ) );
+        threads[ i ].join( );
+    }
+
+	std::vector< int > items;
+
+    for ( int i = 0; i < nThreads; i++ ) {
+		items.insert( std::end( items ), std::begin( itemVectors[ i ] ), std::end( itemVectors[ i ] ) );
+	}
+
+	std::sort( items.begin( ), items.end( ) );
+
+	if ( items.size( ) != nItems ) {
+		std::cout << "test2:  Found " << items.size( ) << " items, but expected " << nItems << std::endl;
+		return false;
+	}
+
+    for ( int i = 0; i < nItems; i++ ) {
+		if ( items[ i ] != i ) {
+			std::cout << "test2:  Somehow, we don't have the items we inserted!" << std::endl;
+			bSuccess = false;
+			break;
+		}
 	}
 
 	return bSuccess;
@@ -71,12 +140,20 @@ bool testInserting( int nThreads, int nItemsPerThread ) {
 
 int main( int argc, char * argv[ ] ) {
 	// step 1, test inserting
-	testInserting( 1, 100 );
-	testInserting( 2, 10000 );
-	testInserting( 10, 10000 );
-	testInserting( 100, 10000 );
-	testInserting( 1000, 10000 );
-	testInserting( 1000, 100000 );
+	std::cout << "Test 1 " << ( testInserting( 1, 100 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 2 " << ( testInserting( 2, 10000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 3 " << ( testInserting( 10, 10000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 4 " << ( testInserting( 100, 10000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 5 " << ( testInserting( 1000, 10000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 6 " << ( testInserting( 10, 1000000 ) ? "is successful" : "failed" ) << std::endl;
+
+	std::cout << "Test 7 " << ( testPopping( 1, 100 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 8 " << ( testPopping( 2, 1000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 10 " << ( testPopping( 10, 10000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 11 " << ( testPopping( 100, 100000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 12 " << ( testPopping( 1000, 1000000 ) ? "is successful" : "failed" ) << std::endl;
+	std::cout << "Test 13 " << ( testPopping( 10000, 10000000 ) ? "is successful" : "failed" ) << std::endl;
+
 	return 0;
 }
 
